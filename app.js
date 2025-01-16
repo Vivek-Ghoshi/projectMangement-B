5867cabe91f5b27031faae89d4e8d1724f6b52da
Vivek ghoshi <the.vivekghoshi@gmail.com>
2025-01-16 09:56:00 +0530

    updated cors configuration

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const db = require('./config/mongoose-connection');
-const authRoutes = require('./routes/authRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-
+const db = require("./config/mongoose-connection");
+const authRoutes = require("./routes/authRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 app.use(cors());
-const allowedOrigins = [
-          'https://projectmanage-f.onrender.com',
-  ];
-  
-  app.use(cors({
+const allowedOrigins = ["https://projectmanage-f.onrender.com"];
+
+app.use(
+  cors({
     origin: (origin, callback) => {
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'));
+        callback(new Error("Not allowed by CORS"));
       }
     },
-    
-  }));
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cors({ origin: 'https://projectmanage-f.onrender.com' }));
-app.use('/api/auth',authRoutes);
-app.use('/api/projects',projectRoutes);
-app.use('/api/tasks',taskRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.listen(3000);
